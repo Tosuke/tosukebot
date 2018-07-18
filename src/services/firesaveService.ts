46313999be305cac44410343cae60a4a000c0b26
Tosuke <tasukeprg@gmail.com>
2018-07-18 18:17:26 +0900

    :wrench: fix escape

--- src/services/firesaveService.ts
+++ src/services/firesaveService.ts
@@ -7,6 +7,7 @@ import axios from 'axios'
 export default (controller: Botkit.SlackController) => {
   controller.on('file_share', async (bot, message) => {
     const file = (message as any).file as File
+    if (file.is_external) return
     const url = await saveFile(file)
     await postToIm(message.user!, {
       username: 'Backup bot',
@@ -26,5 +27,5 @@ async function saveFile(file: File): Promise<string> {
   const bucket = process.env.S3_BUCKET as string
   const objectName = `${file.id}/${file.name}`
   await s3.putObject(bucket, objectName, stream, file.size)
-  return `${process.env.S3_ENDPOINT}/${process.env.S3_BUCKET}/${encodeURI(objectName)}`
+  return `${process.env.S3_ENDPOINT}/${process.env.S3_BUCKET}/${file.id}/${encodeURIComponent(file.name)}`
 }
